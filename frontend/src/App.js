c074810f71b7d7fef3a893dd9e7ce823fdf27c09
akshatsinghania <singhaniaakshat1@gmail.com>
2021-06-27 19:52:55 +0530

    ehance navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,44 +1,43 @@
-import React, { useContext, useEffect } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Home from "./pages/Home/Home";
-import SignIn from "./pages/Auth/SignIn";
-import SignUp from "./pages/Auth/SignUp";
-import NavigationBar from "./components/Navbar/Navbar";
-import { Context } from "./Context/Auth";
-import Discussions from "./pages/Discussions/Discussions";
-import DebateFeed from "./pages/DebateFeed/DebateFeed";
-import Chat from "./pages/Chat/Chat";
-import News from "./news/News";
-
+import React, { useContext, useEffect } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import SignIn from './pages/Auth/SignIn';
+import SignUp from './pages/Auth/SignUp';
+import NavigationBar from './components/Navbar/Navbar';
+import { Context } from './Context/Auth';
+import Discussions from './pages/Discussions/Discussions';
+import DebateFeed from './pages/DebateFeed/DebateFeed';
+import Chat from './pages/Chat/Chat';
+import News from './pages/News/News';
 
 const App = () => {
-  const {
-    auth: { logged },
-  } = useContext(Context);
-  if (logged) {
-    return (
-      <Router>
-        <NavigationBar />
-        <Switch>
-          <Route path="/discussion/:id" component={Chat} />
-          <Route path="/discussion" component={Discussions} />
-          <Route path="/summaries" component={DebateFeed} />
-		  <Route path="/news" component={News} />
-          <Route path="/" component={Home} />
-        </Switch>
-      </Router>
-    );
-  }
-  return (
-    <Router>
-      <NavigationBar />
-      <Switch>
-        <Route path="/signin" component={SignIn} />
-        <Route path="/signup" component={SignUp} />
-        <Route path="/" component={Home} />
-      </Switch>
-    </Router>
-  );
+	const {
+		auth: { logged },
+	} = useContext(Context);
+	if (logged) {
+		return (
+			<Router>
+				<NavigationBar />
+				<Switch>
+					<Route path='/discussion/:id' component={Chat} />
+					<Route path='/discussion' component={Discussions} />
+					<Route path='/summaries' component={DebateFeed} />
+					<Route path='/news' component={News} />
+					<Route path='/' component={Home} />
+				</Switch>
+			</Router>
+		);
+	}
+	return (
+		<Router>
+			<NavigationBar />
+			<Switch>
+				<Route path='/signin' component={SignIn} />
+				<Route path='/signup' component={SignUp} />
+				<Route path='/' component={Home} />
+			</Switch>
+		</Router>
+	);
 };
 
 export default App;
---
+++ frontend/src/pages/News/News.js
@@ -0,0 +1,61 @@
+import React, { useEffect } from 'react';
+import { HomeWrapper } from './News.style';
+import { useState } from 'react';
+import axios from 'axios';
+import NewsContent from './component/newscontent/NewsComponent';
+
+const News = () => {
+	const [category, setCategory] = useState('general');
+	const [articles, setarticles] = useState([]);
+	const [results, setresult] = useState();
+	const [loadMore, setLoadMore] = useState(6);
+	const [newsResults, setNewsResults] = useState();
+
+	const fetchdata = async () => {
+		try {
+			const news = await axios.get(
+				`https://newsapi.org/v2/top-headlines?country=in&apiKey=b5192746245f4d3e83ad09d377381640&pageSize=${loadMore}&category=${category}`,
+			);
+			setarticles(news.data.articles);
+			setresult(news.data.totalResults);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	useEffect(() => {
+		fetchdata();
+	}, [results, loadMore, category]);
+
+	return (
+		<HomeWrapper>
+			<h1 className='title'>Recent NEWS</h1>
+			<center>
+				<button className='ChoiceButton' onClick={() => setCategory('general')}>
+					General
+				</button>
+				<button
+					className='ChoiceButton'
+					onClick={() => setCategory('business')}>
+					Business
+				</button>
+				<button className='ChoiceButton' onClick={() => setCategory('health')}>
+					Health
+				</button>
+				<button
+					className='ChoiceButton'
+					onClick={() => setCategory('technology')}>
+					Technology
+				</button>
+			</center>
+			<NewsContent
+				articles={articles}
+				newsResults={newsResults}
+				loadMore={loadMore}
+				setLoadMore={setLoadMore}
+			/>
+		</HomeWrapper>
+	);
+};
+
+export default News;

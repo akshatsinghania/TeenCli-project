e1ba47411bc1967a4ee962546158ebb1e3554d0b
Arjun-Mishra-312 <arjunmishra31204@gmail.com>
2021-06-27 16:05:50 +0530

    News functionally

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import { HomeWrapper } from './Home.style';
+import React from "react";
+import { HomeWrapper } from "./Home.style";
+import background from "./6602.jpg";
 
 const Home = () => {
-	return (
-		<HomeWrapper>
-			<h1 className='title'>Home</h1>
-		</HomeWrapper>
-	);
+  return (
+    <div>
+      <img src={background} style = {{position:"fixed",width:"100%",height:'100%'}}/>
+	  <h1 style = {{position: 'fixed', left: '25%', top:'25%'}}>Hello</h1>
+    </div>
+  );
 };
 
 export default Home;
